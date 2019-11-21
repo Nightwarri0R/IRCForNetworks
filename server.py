cfad14f52cfebfb3593c10b8fdc576fc32b23e33
NIghtwarri0R <gamena750@abv.bg>
2019-11-21 23:00:05 +0000

    Started working on the channels
    Just did the random port picking option

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 import socket
 import select
+import threading 
+from random import seed
+from random import randint
+
 
 HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
-PORT = 1234
+PORT = []
 # Code found in: https://pythonprogramming.net/server-chatroom-sockets-tutorial-python-3/
 # Create a socket
 # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
 # socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # SO_ - socket option
@@ -16,10 +21,17 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Sets REUSEADDR (as a socket option) to 1 on socket
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+# Random seed to pick numbers which will be used later on to generate sockets
+seed(randint(0 , 30))
+
+for x in range(50):
+    PORT = randint(1000, 5000)
+    
+
 # Bind, so server informs operating system that it's going to use given IP and port
 # For a server using 0.0.0.0 means to listen on all available interfaces, useful to connect locally to 127.0.0.1 and remotely to LAN interface IP
 server_socket.bind((IP, PORT))
-
+print(PORT)
 # This makes server listen to new connections
 server_socket.listen()
 

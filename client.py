c4c95daca534264617d1136161653b03601128db
Neja's Mac <neja.cesnik@hotmail.com>
2019-11-22 11:22:36 +0000

    Added all possible commands on the servers side and !day, !time

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
 PORT = 1234
-my_username = input("Username: ")
+my_username = input("NICK ")
 
 # Create a socket
 # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX

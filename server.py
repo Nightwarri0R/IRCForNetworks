bd9347b0bb697e7fa096f06282f4499491c6c8ba
Costin Grigore <43765185+costingrigore@users.noreply.github.com>
2019-11-27 16:50:06 +0000

    more changes

--- server.py
+++ server.py
@@ -261,65 +261,7 @@ while True:
                 time = datetime.datetime.now()
                 print(time.strftime("%H:%M"))
 
-            # NICK <nickname>
-            # Allows a client to change their IRC nickname.
-            if message["data"].decode("utf-8").find("NICK") != -1:
-                #nickname()
-                break
-
-            # USER
-            elif message["data"].decode("utf-8").find("USER") != -1:
-                #username()
-                break
-
-            # JOIN <channels>
-            # Makes the client join the channels in the comma-separated list <channels>. If the channel(s) do not exist
-            # then they will be created.
-            elif message["data"].decode("utf-8").find("JOIN") != -1:
-                parse = message["data"].decode("utf-8").split(" ", 1)
-                channels = parse[1]
-                #join(channels)
-                break
-
-            # PART <channels>
-            # Causes a user to leave the channels in the comma-separated list <channels>
-            elif message["data"].decode("utf-8").find("PART") != -1:
-                parse = message["data"].decode("utf-8").split(" ", 1)
-                channels = parse[1]
-                #part(channels)
-                break
-
-            # PRIVMSG <msgtarget> <message>
-            # Sends <message> to <msgtarget>, which is usually a user or channel.
-            elif message["data"].decode("utf-8").find("PRIVMSG") != -1:
-                parse = message["data"].decode("utf-8").split(" ", 2)
-                target = parse[1]
-                msg = parse[2]
-                # privatemsg(target, msg)
-
-            # QUIT function
-            elif message["data"].decode("utf-8").find("QUIT") != -1:
-                # sys.exit() HAS TO FINISH THE CLIENT FILE NOT SERVER
-                break
-
-            # DAY function
-            # Shows the date when !day is entered
-            elif message["data"].decode("utf-8").find("!day") != -1:
-                date = datetime.datetime.now()
-                print(date.strftime("%d/%m/%Y"))
-                # Wait for user to input a message
-                #message = date.strftime("%x")
-                # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
-                #message = message.encode('utf-8')
-                #message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-                #client_socket.send(message + message_header)
-
-            # TIME function
-            # Shows the time when !time is entered
-            elif message["data"].decode("utf-8").find("!time") != -1:
-                time = datetime.datetime.now()
-                print(time.strftime("%H:%M"))
-
+           
             # Iterate over connected clients and broadcast message
             for client_socket in clients:
                 # But don't sent it to sender
